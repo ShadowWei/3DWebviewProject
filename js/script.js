394b4bf9397daee857a0f61b911eeb73c88d739a
Hongwei <hongweiyeung@yahoo.com>
2021-01-08 16:10:18 -0700

    Updated 3Dproject
    added some scenes, camer, lights, and BR gltf object

--- js/script.js
+++ js/script.js
@@ -1,12 +1,34 @@
-let scene
+class script {
+
+    constructor() {
+            let scene;
+            let camera;
+            let renderer;
+
+            this.init();
+        }
+        //python -m http.server 8080
+
+    init() {
+        scene = new THREE.scene();
+        scene.background = new THREE.Color(0xdddddd);
+
+        camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 5000);
+
+        light = new THREE.AmbientLight(0x404040, 100);
+
+        scene.add(light);
+
+        renderer = new THREE.WebGLRenderer({ antialias: true });
+        renderer.setSize(window.innerWidth, window.innerHeight);
+
+        document.body.appendChild(renderer.domElement);
 
 
-function init() {
-
-    scene = new THREE.scene();
-    // scene.background = new THREE.color(); 
-    scene.background = new THREE.Color();
-
-
-
+        let loader = new THREE.GLTFLoader();
+        loader.load('scene.gltf', function(gltf) {
+            scene.add(gltf.scene);
+            renderer.render(scene, camera);
+        });
+    }
 }
